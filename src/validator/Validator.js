24bba6aa1a44b7084339bfb107d8c0a478c3a7a2
ccconac <qwerpoiu91@naver.com>
2023-11-15 16:18:44 +0900

    ♻️ refactor: Validator 클래스 메소드 분리

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,6 +1,20 @@
 import { ERROR_MESSAGE } from '../constants/messages';
 
 class Validator {
+  static dateValidator(date) {
+    this.#numberValidator(date);
+    this.#dateRangeValidator(date);
+  }
+
+  static menuValidator(order, menuBoard, orderedMenus) {
+    this.#formValidator(order);
+    this.#menuNameValidator(menuBoard, orderedMenus);
+    this.#menuCountValidator(orderedMenus);
+    this.#duplicateMenuValidator(orderedMenus);
+    this.#totalCountValidator(orderedMenus);
+    this.#onlyDrinkValidator(menuBoard, orderedMenus);
+  }
+
   static #validateNumber(date) {
     return /^[0-9]+$/.test(date);
   }
@@ -45,37 +59,49 @@ class Validator {
     return isOnlyDrink;
   }
 
-  static dateValidator(date) {
+  static #numberValidator(date) {
     if (!Validator.#validateNumber(date)) {
       throw new Error(ERROR_MESSAGE.invalidDate);
     }
+  }
 
+  static #dateRangeValidator(date) {
     if (!Validator.#validateDateRange(date)) {
       throw new Error(ERROR_MESSAGE.invalidDate);
     }
   }
 
-  static menuValidator(order, menuBoard, orderedMenus) {
+  static #formValidator(order) {
     if (!Validator.#validateForm(order)) {
       throw new Error(ERROR_MESSAGE.invalidOrder);
     }
+  }
 
+  static #menuNameValidator(menuBoard, orderedMenus) {
     if (!Validator.#validateMenuName(menuBoard, orderedMenus)) {
       throw new Error(ERROR_MESSAGE.invalidOrder);
     }
+  }
 
+  static #menuCountValidator(orderedMenus) {
     if (!Validator.#validateMenuCount(orderedMenus)) {
       throw new Error(ERROR_MESSAGE.invalidOrder);
     }
+  }
 
+  static #duplicateMenuValidator(orderedMenus) {
     if (!Validator.#validateDuplicateMenu(orderedMenus)) {
       throw new Error(ERROR_MESSAGE.invalidOrder);
     }
+  }
 
+  static #totalCountValidator(orderedMenus) {
     if (Validator.#validateTotalCount(orderedMenus)) {
       throw new Error(ERROR_MESSAGE.invalidTotal);
     }
+  }
 
+  static #onlyDrinkValidator(menuBoard, orderedMenus) {
     if (Validator.#validateOnlyDrink(menuBoard, orderedMenus)) {
       throw new Error(ERROR_MESSAGE.needExtraMenu);
     }
